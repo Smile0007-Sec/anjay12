3a15d51bb94ea1af588f684923a61bb8aea775a3
Smile0007-Sec <70128863+Smile0007-Sec@users.noreply.github.com>
2020-08-25 15:53:46 +0700

    Update and rename .gitignore to Smile0007.sh

---
+++ Smile0007.sh
@@ -0,0 +1,177 @@
+#!/usr/bin/bash
+#tools ini
+#prank hack satelit
+#by Smile0007
+#RECODE GUE ENTOTD LU
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+ungu='\033[1;35m'
+echo " "
+echo  $blue "<|∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆|>"
+echo  $cyan "|       TOOLS HACK satelit-V1        |"
+echo  $red    "|                                    |"
+echo  $red    "| Author        : Smile0007        |"
+echo  $red    "| Contact       : No sistem.       |"
+echo  $red    "| My Team    : Indonesia security sistem |"
+echo  $red    "| Thanks To  : Smile0007.          |"
+echo  $blue "<|∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU HACK $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. ∆Geser Satelit Palapa ∆      $cyan ||"
+echo $blue "|| $green 2. ∆Geser Satelit Telkom∆       $blue ||"
+echo $blue "|| $green 3. ∆Geser Satelit Asiasat∆     $red  ||"
+echo $blue "|| $green 4. ∆Geser Satelit Nasa∆         $green ||"
+echo $blue "|| $green 0. ∆Exit The Program ∆          $purple ||"
+echo $blue "===================================="
+echo $ungu"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@HACK=∆>" Burik;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana cuk]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat CUK]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "1000000000000000000000000%"
+sleep 1
+echo $green "Mampus Down su"
+sleep 1
+echo  $blue "Satelit Berhasi  Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $ungu"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Dah selesai goblok''
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat CUK]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Jan lupa folow akun ig gw jembut "
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana CUK]>"
+read -p"╰───────root@HACK=∆>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@HACK=∆>" derajat;
+sleep 1
+echo $red "LOANJING"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done CUK"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "SELAMAT ANDA TELAH BERHASIL jadi heker"
+echo  $blue "Jangan Lupa Coli Hari Ini CUK...."
+echo  $green "Folow ya ajg "
+exit
+fi
